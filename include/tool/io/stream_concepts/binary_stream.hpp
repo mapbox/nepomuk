52aab39482e6666a7c861888c0fe420a4f369ec6
Moritz Kobitzsch <moritz@mapbox.com>
2017-02-22 14:15:07 +0100

    basic set-up for unit testing

--- include/tool/io/stream_concepts/binary_stream.hpp
+++ include/tool/io/stream_concepts/binary_stream.hpp
@@ -1,6 +1,8 @@
 #ifndef TRANSIT_TOOL_IO_STREAM_CONCEPTS_BINDARY_STREAM_HPP_
 #define TRANSIT_TOOL_IO_STREAM_CONCEPTS_BINDARY_STREAM_HPP_
 
+#include <cstdint>
+
 namespace transit {
 namespace tool {
 namespace io {
@@ -8,11 +10,11 @@ namespace io {
 // Stream Concepts - Where to read bytes from and write bytes to.
 // Models for this could be a readable file, writable memory, etc.
 
-struct InputStream {
+struct BinaryInputStream {
     void ReadBytes(void* into, std::int64_t size);
 };
 
-struct OutputStream {
+struct BinaryOutputStream {
     void WriteBytes(const void* from, std::int64_t size);
 };
 
