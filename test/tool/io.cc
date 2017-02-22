3b19c3f048281aaa3dbc2648b530d7452dd7b487
Moritz Kobitzsch <moritz@mapbox.com>
2017-02-22 14:15:07 +0100

    basic set-up for unit testing

---
+++ test/tool/io.cc
@@ -0,0 +1,9 @@
+#include "tool/io/serialisation.hpp"
+
+// make sure we get a new main function here
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(write_and_read_bytes){
+    BOOST_CHECK_EQUAL(1,1);
+}
