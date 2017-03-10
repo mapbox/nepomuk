86de08aca10aee0aad212ad59ee003ad02e5c219
Moritz Kobitzsch <moritz@mapbox.com>
2017-03-10 17:22:42 +0100

    add timing to get an idea of the performance

--- src/apps/csv-import.cpp
+++ src/apps/csv-import.cpp
@@ -15,6 +15,7 @@
 #include "navigation/algorithm/timetable.hpp"
 #include "search/stop_to_line_factory.hpp"
 #include "tool/container/string_table.hpp"
+#include "tool/timing.hpp"
 
 #include <cstdlib>
 #include <iostream>
@@ -36,13 +37,18 @@ int main(int argc, char **argv) try
         transit::search::StopToLineFactory::produce(dataset.stops.size(), dataset.stop_times);
 
     navigation::algorithm::TimeTable timetable_router(timetable, trip_look_up);
-
-    auto trip = timetable_router(gtfs::Time("00:00:00"), gtfs::StopID{0}, gtfs::StopID{1});
-
     transit::annotation::StopInfoTable stop_info(dataset.stops);
 
+    TIMER_START(query);
+    auto trip = timetable_router(gtfs::Time("00:00:00"), gtfs::StopID{0}, gtfs::StopID{1});
+    TIMER_STOP(query);
+
+    TIMER_START(annotate);
     transit::annotation::Trip const annotator(stop_info, dictionary);
+    TIMER_STOP(annotate);
     std::cout << annotator(trip) << std::endl;
+
+    std::cout << "Query took: " << TIMER_SEC(query) << " seconds, annotation: " << TIMER_SEC(annotate) << std::endl;
 
     return EXIT_SUCCESS;
 }
