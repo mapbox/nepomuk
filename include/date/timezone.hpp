d9e87cdfe5b8620f15f7e2966266fade93d1e2b9
Moritz Kobitzsch <moritz@mapbox.com>
2017-06-23 12:30:15 +0200

    add some commentary on timezone usage

--- include/date/timezone.hpp
+++ include/date/timezone.hpp
@@ -1,9 +1,9 @@
 #ifndef NEPOMUK_DATE_TIMEZONE_HPP_
 #define NEPOMUK_DATE_TIMEZONE_HPP_
 
+#include <boost/optional.hpp>
 #include <cstdint>
 #include <string>
-#include <boost/optional.hpp>
 
 namespace nepomuk
 {
@@ -12,8 +12,10 @@ namespace date
 
 class Timezone
 {
-    public:
-    static boost::optional<std::int32_t> offset(std::string const& timezone);
+  public:
+    // request the standard offsets of a timezone (e.g. America/Los_Angeles) in seconds. To be able
+    // to use this, make sure to set-up the ICU via `icu::init` as part of `icu.hpp`
+    static boost::optional<std::int32_t> offset(std::string const &timezone);
 };
 
 } // namespace date
